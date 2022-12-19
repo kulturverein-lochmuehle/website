7eaa6ed46b628da3d0f7f0849df7c7ac09e69968
David Enke <david@enke.dev>
2022-12-19 23:59:20 +0100

    added menu arrow

--- packages/frontend/src/components/features/navigation/navigation/navigation.component.ts
+++ packages/frontend/src/components/features/navigation/navigation/navigation.component.ts
@@ -36,7 +36,16 @@ export class Navigation extends LitElement {
   render() {
     return html`
       <nav>
-        <a href="/"><kvlm-logo></kvlm-logo></a>
+        <a href="/">
+          <kvlm-logo></kvlm-logo>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30.556 20.795">
+            <path
+              d="M927.335,4843.929l13.731,16.4,13.76-16.4"
+              transform="translate(-925.801 -4842.643)"
+            />
+          </svg>
+        </a>
         <slot></slot>
       </nav>
     `;
