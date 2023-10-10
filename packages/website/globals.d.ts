9ea29509ac5dd5b6ec8b62ea1a426d3a527614b7
david@kvlm.de <david@kvlm.de>
2023-10-10 23:06:01 +0200

    updated dependencies

--- packages/website/globals.d.ts
+++ packages/website/globals.d.ts
@@ -1,3 +1,7 @@
 declare module '@astrojs/netlify/functions' {
   export { default } from '@astrojs/netlify/dist/integration-functions';
 }
+
+declare module '@astrojs/netlify/edge-functions' {
+  export { default } from '@astrojs/netlify/dist/integration-edge-functions';
+}
