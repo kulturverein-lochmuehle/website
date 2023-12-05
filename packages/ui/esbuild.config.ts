f7a9daecb17b57b87b2b9c340cf3d72317449301
david@kvlm.de <david@kvlm.de>
2023-12-05 22:18:11 +0100

    build: remove browser sync

--- packages/ui/esbuild.config.ts
+++ packages/ui/esbuild.config.ts
@@ -1,25 +1,14 @@
 /* eslint-disable no-console */
-import { createServer, request as httpRequest } from 'node:http';
 import { resolve } from 'node:path';
 import { parseArgs } from 'node:util';
 
 import { type BuildOptions, build, context } from 'esbuild';
-import { sassPlugin } from 'esbuild-sass-plugin';
+import { sassPlugin, type SassPluginOptions } from 'esbuild-sass-plugin';
 
 import glob from 'fast-glob';
 import autoprefixer from 'autoprefixer';
 import postcss from 'postcss';
-// import postcssPresetEnv from 'postcss-preset-env';
-
-import { browserSyncPlugin } from './esbuild-plugin-browser-sync.js';
-
-// apply postcss with autoprefixer in sass
-const transform = async (source: string): Promise<string> => {
-  const { css } = await postcss([autoprefixer /*postcssPresetEnv({ stage: 0 })*/]).process(source, {
-    from: source
-  });
-  return css;
-};
+import postcssPresetEnv from 'postcss-preset-env';
 
 // resolve @ imports in sass
 const importMapper = (path: string): string => {
@@ -28,12 +17,34 @@ const importMapper = (path: string): string => {
   return path;
 };
 
+// add postcss to sass transformer
+const transform = async (source: string): Promise<string> => {
+  const { css } = await postcss([autoprefixer, postcssPresetEnv({ stage: 0 })]).process(source, {
+    from: source
+  });
+  return css;
+};
+
+// will load suffixed paths as strings inline to be used in web components
+export const inlineOptions: SassPluginOptions = {
+  type: 'css-text',
+  filter: /\.component\.scss$/,
+  importMapper,
+  transform
+};
+
+// will transform the given SCSS files into CSS files
+export const globalOptions: SassPluginOptions = {
+  type: 'css',
+  importMapper,
+  transform
+};
+
 // parse cli arguments
 const {
-  values: { ci = false, port: targetPort = '3500', watch }
+  values: { port = '3500', watch }
 } = parseArgs({
   options: {
-    ci: { type: 'boolean' },
     port: { type: 'string', short: 'p' },
     watch: { type: 'boolean', short: 'w' }
   }
@@ -62,7 +73,6 @@ const options: BuildOptions = {
   metafile: true,
   minify: true,
   treeShaking: true,
-  // external: ['lit*'],
   sourcemap: watch ? 'both' : false,
   loader: {
     '.html': 'copy',
@@ -76,87 +86,16 @@ const options: BuildOptions = {
     '.woff2': 'file'
   },
   logLevel: watch ? 'info' : 'error',
-  plugins: [
-    sassPlugin({
-      type: 'css-text',
-      filter: /\.component\.scss$/,
-      importMapper,
-      transform
-    }),
-    sassPlugin({
-      type: 'css',
-      importMapper,
-      transform
-    })
-  ]
+  plugins: [sassPlugin(inlineOptions), sassPlugin(globalOptions)]
 };
 
 if (watch) {
-  try {
-    const bannerJs = ` if (typeof EventSource !== 'undefined') { new EventSource('/esbuild').addEventListener('change', () => location.reload()) }`;
-    const green = (message: string) => (ci ? message : `\u001b[32m${message}\u001b[0m`);
-    const grey = (message: string) => (ci ? message : `\u001b[90m${message}\u001b[0m`);
-    const cyan = (message: string) => (ci ? message : `\u001b[36m${message}\u001b[0m`);
+  const bannerJs = ` if (typeof EventSource !== 'undefined') { new EventSource('/esbuild').addEventListener('change', () => location.reload(true)) }`;
 
-    // start dev server in watch mode
-    const ctx = await context({
-      ...options,
-      banner: { js: bannerJs },
-      plugins: [
-        ...(options.plugins ?? []),
-        browserSyncPlugin({
-          logLevel: 'silent',
-          logPrefix: 'KVLM',
-          logSnippet: false,
-          codeSync: false,
-          reloadOnRestart: false,
-          injectChanges: false,
-          single: true,
-          ghostMode: {
-            clicks: true,
-            forms: true,
-            // @ts-expect-error this seems not to be typed properly
-            location: true,
-            scroll: true
-          },
-          onReady(sync) {
-            const syncPort = sync.getOption('ui').get('port');
-            const syncUrl = `http://0.0.0.0:${syncPort}`;
-            console.clear();
-            console.info(grey('  KVLM'));
-            console.info(`${green('>')} Browser Sync UI started at ${cyan(syncUrl)}`);
-          }
-        })
-      ]
-    });
-    await ctx.watch();
-    const { host: hostname, port } = await ctx.serve({ servedir: 'dist' });
-
-    // use proxy for SPA
-    // https://esbuild.github.io/api/#serve-proxy
-    createServer((request, response) => {
-      const { url, method, headers } = request;
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const path = ~url!.split('/').pop()!.indexOf('.') || url === '/esbuild' ? url : '/index.html';
-
-      request.pipe(
-        httpRequest({ hostname, port, path, method, headers }, proxyReponse => {
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          response.writeHead(proxyReponse.statusCode!, proxyReponse.headers);
-          proxyReponse.pipe(response, { end: true });
-        }),
-        { end: true }
-      );
-    }).listen(targetPort);
-
-    // notify user
-    console.info(`${green('>')} Server started at ${cyan(`http://${hostname}:${port}`)}`);
-    console.info(`${green('>')} Proxy started at ${cyan(`http://${hostname}:${targetPort}`)}`);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+  // start dev server in watch mode
+  const ctx = await context({ ...options, banner: { js: bannerJs } });
+  await ctx.watch();
+  await ctx.serve({ servedir: 'dist', port: Number(port) });
 } else {
   await build(options);
-  process.exit(0);
 }
