8894a5af58a8d8550a5e409fdf872c933f877779
David Enke <david@kvlm.de>
2024-11-20 10:36:58 +0100

    chore: add collection descriptions

--- packages/website/src/collections/navigation.schema.ts
+++ packages/website/src/collections/navigation.schema.ts
@@ -4,9 +4,9 @@ export const schema = z.object({
   pages: z
     .array(
       z.object({
-        page: z.string().describe('Page'),
-        useSections: z.boolean().nullish().default(false).describe('Use Sections'),
+        page: z.string().describe('Seite'),
+        useSections: z.boolean().nullish().default(false).describe('Abschnitte verwenden'),
       }),
     )
-    .describe('Pages'),
+    .describe('Seiten'),
 });
