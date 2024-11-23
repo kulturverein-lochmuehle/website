a71815e6a3f750d51e9a2bc2172154e9508b7c58
David Enke <david@kvlm.de>
2024-11-23 13:23:31 +0100

    feat: util for unique global style injection

---
+++ packages/ui/src/utils/crypto.utils.ts
@@ -0,0 +1,7 @@
+export async function hashFrom(input: string): Promise<string> {
+  const data = new TextEncoder().encode(input);
+  const hash = await window.crypto.subtle.digest('SHA-256', data);
+  return Array.from(new Uint8Array(hash))
+    .map(b => b.toString(16).padStart(2, '0'))
+    .join('');
+}
