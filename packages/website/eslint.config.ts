5efd2f456aa3624427b4baa763f02da5be3480a7
David Enke <david@kvlm.de>
2025-07-14 22:00:19 +0200

    chore(website): configure linting

--- packages/website/eslint.config.ts
+++ packages/website/eslint.config.ts
@@ -1,13 +1,25 @@
-import config from '@enke.dev/lint';
+import baseConfig from '@enke.dev/lint';
+import type { FixupConfigArray } from '@eslint/compat';
+import { fixupConfigRules } from '@eslint/compat';
 import { FlatCompat } from '@eslint/eslintrc';
-import type { Linter } from 'eslint';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 
-const baseDirectory = dirname(fileURLToPath(import.meta.url));
-const compat = new FlatCompat({ baseDirectory });
+// Get Next.js configs and fix compatibility issues
+const nextConfigs = fixupConfigRules(
+  new FlatCompat({ baseDirectory: import.meta.dirname }).extends(
+    'next/core-web-vitals',
+    'next/typescript',
+  ) as FixupConfigArray,
+);
 
-export default [
-  ...config,
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-] satisfies Linter.Config[];
+const eslintConfig = [
+  ...nextConfigs,
+  ...baseConfig.filter(config => {
+    const knownCriticalPlugins = ['import', '@typescript-eslint', 'json'];
+    // Skip configs that define plugins that might conflict with Next.js
+    return Object.keys(config.plugins ?? {}).every(
+      plugin => !knownCriticalPlugins.includes(plugin),
+    );
+  }),
+];
+
+export default eslintConfig;
--- packages/website/next.config.ts
+++ packages/website/next.config.ts
@@ -1,7 +1,11 @@
+import { join } from 'node:path';
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  outputFileTracingRoot: join(__dirname, '../../'),
 };
 
 export default nextConfig;
