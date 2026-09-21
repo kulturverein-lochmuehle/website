7fbc8c790b10219dd79cdae93a9df432700e12cd
David Enke <david@kvlm.de>
2026-09-21 22:15:22 +0200

    style: unstick section headlines, pin the footer on desktop
    - the teaser headline scrolls with its content again
    - the footer sticks to the bottom of the viewport from `lg` upwards,
      below that it would take too much of the screen

--- packages/website/markdoc.config.ts
+++ packages/website/markdoc.config.ts
@@ -3,7 +3,4 @@ import { defineMarkdocConfig } from '@astrojs/markdoc/config';
 import { nodes } from './src/markdoc/nodes.js';
 import { tags } from './src/markdoc/tags.js';
 
-export default defineMarkdocConfig({
-  nodes,
-  tags,
-});
+export default defineMarkdocConfig({ nodes, tags });
